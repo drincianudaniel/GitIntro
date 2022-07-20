a0a4b87fcdc490640d3d8bc0cd1d571d93e42972
Dani <drincianu.dani@gmail.com>
2022-07-20 15:25:44 +0300

    Sum Function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, User1!");
+
+int test(int x, int y)
+{
+    return x + y;
+}
+
+Console.WriteLine(test(2,3));
